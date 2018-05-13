2a0d8028c48ad627f0e7719e875f41a30a2d83ae
J. Graves <jwgravesfl@live.com>
2018-05-13 13:21:33 -0400

    first commit of React Native Inventory Manager

--- App.js
+++ App.js
@@ -1,22 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native'
+import {Constants} from 'expo'
+
+let id = 0
+
+const styles = StyleSheet.create({
+  inventoryContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  appContainer: {
+    paddingTop: Constants.statusBarHeight,
+  },
+  fill: {
+    flex: 1,
+  }
+})
+
+const Inventory = props => (
+  <View style={styles.inventoryContainer}>
+    <Switch value={props.inventory.checked} onValueChange={props.onToggle} />
+    <Button onPress={props.onDelete} title="delete" />
+    <Text>{props.inventory.text}</Text>
+  </View>
+)
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      inventories: [],
+    }
+  }
+
+  addInventory() {
+    id++
+    const text = `Inventory number ${id}`
+    this.setState({
+      inventories: [
+        ...this.state.inventories,
+        {id: id, text: text, checked: false},
+      ], 
+    })
+  }
+
+  removeInventory(id) {
+    this.setState({
+      inventories: this.state.inventories.filter(inventory => inventory.id !== id)
+    })
+  }
+
+  toggleInventory(id) {
+    this.setState({
+      inventories: this.state.inventories.map(inventory => {
+        if (inventory.id !== id) return inventory
+        return {
+          id: inventory.id,
+          text: inventory.text,
+          checked: !inventory.checked,
+        }
+      })
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>What up Big Div!</Text>
-        
+      <View style={[styles.appContainer, styles.fill]}>
+        <Text>Inventory count: {this.state.inventories.length}</Text>
+        <Text>Unchecked inventory count: {this.state.inventories.filter(inventory => !inventory.checked).length}</Text>
+        <Button onPress={() => this.addInventory()} title="Add Inventory" />
+        <ScrollView style={styles.fill}>
+          {this.state.inventories.map(inventory => (
+            <Inventory
+              onToggle={() => this.toggleInventory(inventory.id)}
+              onDelete={() => this.removeInventory(inventory.id)}
+              inventory={inventory}
+            />
+          ))}
+        </ScrollView>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
